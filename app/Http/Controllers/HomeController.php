4f068b2761d9832c24f671ffd0b2a004b4567a33
jyostna <ju36@njit.edu>
2018-05-01 01:24:55 -0400

    added comment

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,7 +30,7 @@ class HomeController extends Controller
         $user = Auth::user();
         $questions = $user->questions()->paginate(6);
 
-
+//Test comment
         //dd($questions);
         return view ('home')->with('questions',$questions);
         //return view('home', ['questions' => $questions, 'latestQuestions' =>  $latestQuestions ]);
