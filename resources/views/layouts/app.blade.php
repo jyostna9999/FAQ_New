aed1459d068c77305767440309d9ce3fb50a8a49
jyostna <ju36@njit.edu>
2018-05-01 10:55:14 -0400

    Working changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -105,17 +105,8 @@
 </div>
 
 <div class="container-fluid">
-<aside class="sidebar-module  ml-2 mr-2">
-    <div class="p-3 mb-3 bg-light rounded">
-        <h5 class="font-weight-normal">Recent Questions</h5>
 
-
-        <ol class="list-unstyled mb-0">
-            <li><a href="#">Latest Question</a></li>
-
-        </ol>
-    </div>
-</aside>
+    @include('sidebar')
 </div>
 </body>
 
--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -1,5 +1,11 @@
-@extends('layouts.app')
+<aside class="sidebar-module  ml-2 mr-2">
+    <div class="p-3 mb-3 bg-light rounded">
+        <h5 class="font-weight-normal">Recent Questions</h5>
 
-@section('content')
 
-@endsection
+        <ol class="list-unstyled mb-0">
+            <li><a href="#">Latest Question</a></li>
+
+        </ol>
+    </div>
+</aside>
